0ea8228c73a85a7942ca74cf8b32d028d576e4fc
rlatkddn212 <rlatkddn212@naver.com>
2024-08-20 16:30:59 +0900

    build.cs 모듈 라이브러리 추가

--- Source/HighSocietyMN/HighSocietyMN.Build.cs
+++ Source/HighSocietyMN/HighSocietyMN.Build.cs
@@ -8,7 +8,8 @@ public class HighSocietyMN : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput"
+		, "UMG", "HTTP", "Json", "JsonUtilities" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
